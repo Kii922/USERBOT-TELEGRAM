e2096c3a4cb8c95bab8c4cacee45f385c56a6f6a
Ripki <riivki727@gmail.com>
2026-06-20 22:01:05 +0700

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,10 +1,14 @@
 from handlers.groups import register_group_handlers
 from handlers.send import register_send_handlers
 from handlers.help import register_help_handlers
+from handlers.schedule import register_schedule_handlers
+from handlers.register import register_register_handlers
 
 
-def register_all_handlers(app):
+def register_all_handlers(app, owner_id):
     """Register semua command handlers."""
     register_help_handlers(app)
     register_group_handlers(app)
     register_send_handlers(app)
+    register_schedule_handlers(app)
+    register_register_handlers(app, owner_id)
